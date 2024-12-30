82652af19807b40fb906bc9eff03e2a19a84b93a
Chaoyang Shen <chaoyangshen119@gmail.com>
2024-12-30 15:42:10 -0800

    clean up footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,25 +5,6 @@ export default function Footer() {
   return (
     <footer class="footer">
       <div>
-        {/* <div class="footer__title">
-            <h1>Get in Touch</h1>
-
-            <h1 class="footer__logo1">Sweat and Syntax</h1>
-          </div>
-
-          <div class="footer__address">
-            <span>
-              <h2>Authors</h2>
-              <p>names</p>
-              <p>names</p>
-              <p>names</p>
-              <p>names</p>
-              <a href="mailto:shec@seas.upenn.edu" class="email">
-                shec@seas.upenn.edu
-              </a>
-            </span>
-          </div> */}
-
         <p class="copyright">
           Copyright Sweat and Syntax © 2024 All Rights Reserved
         </p>
